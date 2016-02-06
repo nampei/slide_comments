97a97c328255868ed4fe704151fa07fb1d82990c
nampei kamata <k0408n@yahoo.co.jp>
2016-02-06 21:47:22 +0900

    socket

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ var io = require('socket.io')(server);
 
 server.listen(80);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
