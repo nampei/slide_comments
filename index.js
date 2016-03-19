f0197f9ba46fc534a36a27c164ace2db59bc8c82
madobon <mimicopy@live.jp>
2016-03-19 11:09:54 +0900

    marquee update

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   socket.on('chat', function(chat) {
+    console.log(JSON.parse(chat));
     socket.broadcast.emit('broad chat', chat);
   });
 
