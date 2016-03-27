9959f46a128006e123b8c182c5f5a53fc17162d0
nampei kamata <k0408n@yahoo.co.jp>
2016-03-27 18:26:21 +0900

    room_client and comment_client

--- db/redis.js
+++ db/redis.js
@@ -1,28 +1,67 @@
 var redis = require("redis");
+var crypto = require('crypto');
 
-client = redis.createClient();
-client.flushdb();
+// リスト型 <key: room_id, value: comment_id, comment_id, comment_id, ...>
+room_client = redis.createClient();
 
-var room = "hogeroom";
-var user_id = "1";
-var comment = "hogecomment1";
-var user_id2 = "2";
-var comment2 = "hogecomment2"
+// ハッシュ型 <key: comment_id, value: user_id: "1", comment_text: "こんにちは", datetime: 346732362116>
+comment_client = redis.createClient();
 
-function set_comment(room, user_id, comment) {
-  client.hmset(room, 'user_id', user_id, 'comment', comment);
+// 初期化
+comment_client.flushdb();
+room_client.flushdb();
+
+function getrandom() {
+  var current_date = (new Date()).valueOf().toString();
+  var random = Math.random().toString();
+  var hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+  return hash;
 }
 
-function get_room_comments(room) {
-  client.hgetall(room, function(err, obj) {
-    console.log("room: " + room);
+var room_id = "1";
+
+var comment1 = {
+  comment_id: getrandom(),
+  user_id: "1",
+  comment_text: "comment_text1",
+  datetime: (new Date()).valueOf().toString()
+}
+var comment2 = {
+  comment_id: getrandom(),
+  user_id: "2",
+  comment_text: "comment_text2",
+  datetime: (new Date()).valueOf().toString()
+}
+
+function push_comment_id(comment_id) {
+  room_client.lpush(room_id, comment_id);
+}
+
+function set_comment(comment) {
+  comment_client.hmset(
+    comment.comment_id,
+    'user_id', comment.user_id,
+    'comment_text', comment.comment_text,
+    "datetime", comment.datetime
+  );
+}
+
+function get_room_comments(comment) {
+  comment_client.hgetall(comment, function(err, obj) {
+    console.log("comment_id: " + comment);
     console.log(obj);
   });
 }
 
-set_comment(room, user_id, comment);
-get_room_comments(room);
-set_comment(room, user_id2, comment2);
-get_room_comments(room);
+push_comment_id(comment1.comment_id);
+set_comment(comment1);
+get_room_comments(comment1.comment_id);
+push_comment_id(comment2.comment_id);
+set_comment(comment2);
+get_room_comments(comment2.comment_id);
+room_client.lrange(room_id, 0, -1, function(err, obj) {
+  console.log("get comment_id list "+ room_id +": " + obj);
+});
 
-client.quit();
+comment_client.quit();
+room_client.quit();
